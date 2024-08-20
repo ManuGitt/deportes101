c7b24f4b223244bd31650610df6edea4d2242d41
ManuGitt <111716796+ManuGitt@users.noreply.github.com>
2024-08-20 17:52:06 -0300

    Add files via upload
    Añadida la sección de productos destacados. Añadidos los iconos de la navbar.

--- index.php
+++ index.php
@@ -20,20 +20,34 @@
     </div>
   </div>
 
+  
+  <?php $sentenciaSQL = $conexion -> prepare("SELECT * FROM productos");
+        $sentenciaSQL -> execute();
+        $listaProductos = $sentenciaSQL->fetchAll(PDO::FETCH_ASSOC);
+        $sentenciaSQL2 = $conexion -> prepare("SELECT * FROM productos ORDER BY cantidadComprada DESC LIMIT 6");
+        $sentenciaSQL2 -> execute();
+        $productosDestacados = $sentenciaSQL2 ->fetchAll(PDO::FETCH_ASSOC);
+  ?>
 
   <section class="featuredProducts-section">
     <h2 class="title">Productos destacados</h2>
     <div class="featuredProducts-row">
 
+    <?php foreach ($productosDestacados as $producto) { ?>
+      <a href="">
         <div class="productCard">
-          <img class="productCard-img" src="./img/img-products/zapatillasPrueba.jpg" alt="">
-          <div class="productCard-body">
-            <h3 class="productCard-title"></h3>
-            <p class="productCard-price"></p>
-          </div>
+            <img class="productCard-img" src="./img/img-products/zapatillasPrueba.jpg" alt="">
+            <div class="productCard-body">
+              <h3 class="productCard-title"><?php echo $producto['nombre_producto'] ?></h3>
+              <p class="productCard-price">$<?php echo $producto['precio_producto'] ?></p>
+            </div>
         </div>
+      </a>
+    <?php }?>
 
     </div>
   </section>
 </main>
+<img src="./img/img-products/zapatillasPrueba.jpg" alt="">
+
 <?php include('./template/footer.php') ?>
--- template/header.php
+++ template/header.php
@@ -1,3 +1,4 @@
+<?php include('./conection.php') ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,23 +14,60 @@
             <div class="nav-principal">
                 <ul>
                     <li id="logo"><a href=""><img src="img/img-web/logobase.jpg" alt=""></a></li>
+                    
                     <li id="searchbar">
-                        <div>
-                            <img src="img/img-web/lupita.png" alt="">
+                        <div class="searcher">
+                            <img src="img/img-web/pngegg.png" alt="">
                         </div>
                         <input type="text">
                     </li>
-                    <li id="carrito"></li>
-                    <li id="account"></li>
+
+                    <li id="carrito" class="navbar-item"><img src="img/img-web/MdiCart.png" alt=""></li>
+                    <li id="account" class="navbar-item"><img src="img/img-web/PhUserCircle.png" alt=""></li>
                 </ul>
             </div>
+
             <div class="nav-secundaria">
-                <ul>
-                    <li><a href="">MARCAS</a></li>
-                    <li><a href="">DEPORTES</a></li>
-                    <li><a href="">ACCESORIOS</a></li>
-                    <li><a href="">INDUMENTARIA</a></li>
-                </ul>
+            <ul>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">MARCAS</a>
+                <div class="dropdown-content">
+                    <?php 
+                    $sentenciaSQL = $conexion -> prepare("SELECT * FROM marcas");
+                    $sentenciaSQL -> execute();
+                    $marcas = $sentenciaSQL ->fetchAll(PDO::FETCH_ASSOC);
+
+                    foreach ($marcas as $marca) {
+                    ?>
+                    <a href="#"><?php echo $marca['nombre_marca'] ?></a>
+                    <?php }?>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">DEPORTES</a>
+                <div class="dropdown-content">
+                    <a href="#">Deporte 1</a>
+                    <a href="#">Deporte 2</a>
+                    <a href="#">Deporte 3</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">ACCESORIOS</a>
+                <div class="dropdown-content">
+                    <a href="#">Accesorio 1</a>
+                    <a href="#">Accesorio 2</a>
+                    <a href="#">Accesorio 3</a>
+                </div>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">INDUMENTARIA</a>
+                <div class="dropdown-content">
+                    <a href="#">Indumentaria 1</a>
+                    <a href="#">Indumentaria 2</a>
+                    <a href="#">Indumentaria 3</a>
+                </div>
+            </li>
+        </ul>
             </div>
         </nav>
     </header>
